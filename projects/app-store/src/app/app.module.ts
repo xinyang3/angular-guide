fff286dc139314649f124107fba703dd8e639b0d
xinyang3 <xinyang.zhu@outlook.com>
2020-04-11 19:11:30 +0800

    快速上手-数据service

--- projects/app-store/src/app/app.module.ts
+++ projects/app-store/src/app/app.module.ts
@@ -1,26 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from '../components/product-list/product-list.component';
 import { ProductAlertsComponent } from '../components/product-alerts/product-alerts.component';
 import { ProductDetailComponent } from '../components/product-detail/product-detail.component';
+import { CartComponent } from '../components/cart/cart.component';
+import { ShippingComponent } from '../components/shipping/shipping.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: 'products/:productId', component: ProductDetailComponent },
       { path: '', component: ProductListComponent },
+      { path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponent}
     ])
   ],
   providers: [],
--- projects/app-store/src/components/product-detail/product-detail.component.ts
+++ projects/app-store/src/components/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../product-list/products'
+import { products } from '../product-list/products';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,12 +11,17 @@ import { products } from '../product-list/products'
 export class ProductDetailComponent implements OnInit {
   product;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')]
     })
+  }
+
+  addToCart(product) {
+    // window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
   }
 
 }
