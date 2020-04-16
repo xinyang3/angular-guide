87248749532a3967640e4e8983c3cbbca258d042
xinyang3 <zhu.xinyang@outlook.com>
2020-04-16 12:13:20 +0800

    英雄编辑器

---
+++ projects/angular-tour-heroes/src/components/heroes-component/heroes-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroesComponentComponent } from './heroes-component.component';
+
+describe('HeroesComponentComponent', () => {
+  let component: HeroesComponentComponent;
+  let fixture: ComponentFixture<HeroesComponentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeroesComponentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroesComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/angular-tour-heroes/src/components/heroes-component/heroes-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-component',
+  templateUrl: './heroes-component.component.html',
+  styleUrls: ['./heroes-component.component.css']
+})
+export class HeroesComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
